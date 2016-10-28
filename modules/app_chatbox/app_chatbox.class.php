0c7cd6943c72a986faea47d458a47e29b64c4b2e
nick7zmail <nick7zmail@gmail.com>
2016-10-28 17:51:41 +0500

    master branch publish
    publish

---
+++ modules/app_chatbox/app_chatbox.class.php
@@ -0,0 +1,244 @@
+<?php
+/**
+* chat 
+*
+* chat
+*
+* @package MajorDoMo
+* @author Serge Dzheigalo <jey@tut.by> http://smartliving.ru/
+* @version 0.3 (wizard, 23:01:48 [Jan 30, 2007])
+*/
+//
+//
+class app_chatbox extends module {
+/**
+* chat
+*
+* Module class constructor
+*
+* @access private
+*/
+function app_chatbox() {
+  $this->name="app_chatbox";
+  $this->title="ChatBox";
+  $this->module_category="<#LANG_SECTION_APPLICATIONS#>";
+  $this->checkInstalled();
+}
+/**
+* saveParams
+*
+* Saving module parameters
+*
+* @access public
+*/
+function saveParams($data=1) {
+ $data=array();
+ if (IsSet($this->id)) {
+  $data["id"]=$this->id;
+ }
+ if (IsSet($this->view_mode)) {
+  $data["view_mode"]=$this->view_mode;
+ }
+ if (IsSet($this->edit_mode)) {
+  $data["edit_mode"]=$this->edit_mode;
+ }
+ if (IsSet($this->tab)) {
+  $data["tab"]=$this->tab;
+ }
+ return parent::saveParams($data);
+}
+/**
+* getParams
+*
+* Getting module parameters from query string
+*
+* @access public
+*/
+function getParams($data = 1) {
+  global $id;
+  global $mode;
+  global $view_mode;
+  global $edit_mode;
+  global $tab;
+  if (isset($id)) {
+   $this->id=$id;
+  }
+  if (isset($mode)) {
+   $this->mode=$mode;
+  }
+  if (isset($view_mode)) {
+   $this->view_mode=$view_mode;
+  }
+  if (isset($edit_mode)) {
+   $this->edit_mode=$edit_mode;
+  }
+  if (isset($tab)) {
+   $this->tab=$tab;
+  }
+}
+
+/**
+* Run
+*
+* Description
+*
+* @access public
+*/
+function run() {
+ global $session;
+  if ($this->action=='admin') {
+   $this->admin($out);
+  } else {
+   $this->usual($out);
+  }
+  if (IsSet($this->owner->action)) {
+   $out['PARENT_ACTION']=$this->owner->action;
+  }
+  if (IsSet($this->owner->name)) {
+   $out['PARENT_NAME']=$this->owner->name;
+  }
+  $out['VIEW_MODE']=$this->view_mode;
+  $out['EDIT_MODE']=$this->edit_mode;
+  $out['MODE']=$this->mode;
+  $out['ACTION']=$this->action;
+  $out['TAB']=$this->tab;
+
+  $res=SQLSelect("SELECT * FROM users ORDER BY ID");
+  $textdata ='
+                            <div class=\"table-responsive\">
+                                <table class=\"table\">
+                                    <thead>
+										<tr>
+											<th>ID</th>
+											<th>ИМЯ</th>
+										</tr>
+									</thead>';
+  $total=count($res);
+  for($i=0;$i<$total;$i++) {
+	$textdata .= "					
+									<tbody>
+										<tr>
+											<td>".$res[$i]['ID']."</td>
+											<td>".$res[$i]['NAME']."</td>
+										</tr>\n";
+  }
+  $textdata .= '
+									</tbody>
+								</table>
+							</div>';
+  $out['TXT_ADMIN']=$textdata;
+
+  if ($this->single_rec) {
+   $out['SINGLE_REC']=1;
+  }
+  $this->data=$out;
+  $p=new parser(DIR_TEMPLATES.$this->name."/".$this->name.".html", $this->data, $this);
+  $this->result=$p->result;
+}
+
+
+
+/**
+* BackEnd
+*
+* Module backend
+*
+* @access public
+*/
+function admin(&$out) {
+
+ global $session;
+
+ if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
+  $out['SET_DATASOURCE']=1;
+ }
+
+
+
+
+// echo $textdata;
+
+ if ($this->data_source=='shouts' || $this->data_source=='') {
+    if ($this->view_mode=='' || $this->view_mode=='search_shouts') {
+	$this->search_shouts($out);
+    }
+    if ($this->view_mode=='delete_shouts') {
+	$this->delete_shouts($this->id);
+	$this->redirect("?");
+    }
+ }
+}
+
+
+/**
+* FrontEnd
+*
+* Module frontend
+*
+* @access public
+*/
+function usual(&$out) {
+ SQLExec("DELETE FROM shouts WHERE (TO_DAYS(NOW())-TO_DAYS(ADDED))>7");
+ $this->admin($out);
+}
+
+/**
+* shouts search
+*
+* @access public
+*/
+ function search_shouts(&$out) {
+  require(DIR_MODULES.$this->name.'/chat_search.inc.php');
+ }
+
+
+/**
+* Install
+*
+* Module installation routine
+*
+* @access private
+*/
+ function install($parent_name="") {
+  parent::install($parent_name);
+ }
+/**
+* Uninstall
+*
+* Module uninstall routine
+*
+* @access public
+*/
+ function uninstall() {
+  SQLExec('DROP TABLE IF EXISTS shouts');
+  parent::uninstall();
+ }
+/**
+* dbInstall
+*
+* Database installation routine
+*
+* @access private
+*/
+ function dbInstall($data) {
+/*
+shouts - chat
+*/
+  $data = <<<EOD
+ shouts: ID int(10) unsigned NOT NULL auto_increment
+ shouts: ROOM_ID int(10) NOT NULL DEFAULT '0'
+ shouts: MEMBER_ID int(10) NOT NULL DEFAULT '0'
+ shouts: MESSAGE varchar(255) NOT NULL DEFAULT ''
+ shouts: IMPORTANCE int(10) NOT NULL DEFAULT '0'
+ shouts: ADDED datetime
+EOD;
+  parent::dbInstall($data);
+ }
+// --------------------------------------------------------------------
+}
+/*
+*
+* TW9kdWxlIGNyZWF0ZWQgSmFuIDMwLCAyMDA3IHVzaW5nIFNlcmdlIEouIHdpemFyZCAoQWN0aXZlVW5pdCBJbmMgd3d3LmFjdGl2ZXVuaXQuY29tKQ==
+*
+*/
+?>
---
+++ modules/app_chatbox/chat_search.inc.php
@@ -0,0 +1,195 @@
+<?php
+/*
+* @version 0.2 (auto-set)
+*/
+
+  global $session;
+  global $msg;
+  global $getdata;
+  global $clear;
+
+//#######################################################################################################
+  if ($this->action=='admin' && $clear) {
+   SQLExec("DELETE FROM shouts");
+   $this->redirect("?");
+  }
+//#######################################################################################################
+  if (!$session->data['SITE_USERNAME']) {
+   $out['NOT_LOGGED']=1;
+  } else {
+   $user=SQLSelectOne("SELECT * FROM users WHERE USERNAME='".DBSafe($session->data['SITE_USERNAME'])."'");
+   $session->data['logged_user']=$user['ID'];
+  }
+
+//#######################################################################################################
+  if ($this->action=='' && $session->data['logged_user'] && $msg!='') {
+   $rec=array();
+   $rec['MEMBER_ID']=$session->data['logged_user'];
+   $rec['MESSAGE']=htmlspecialchars($msg);
+   $rec['ADDED']=date('Y-m-d H:i:s');
+   SQLInsert('shouts', $rec);
+
+   include_once(DIR_MODULES.'patterns/patterns.class.php');
+   $pt=new patterns();
+
+   $res=$pt->checkAllPatterns($rec['MEMBER_ID']);
+   if (!$res) {
+    processCommand($msg);
+   }
+   $getdata=1;
+  }
+//#######################################################################################################
+
+  if ($this->owner->name=='panel') {
+   $out['CONTROLPANEL']=1;
+  }
+//  $qry="1";
+
+  if ($this->action!='admin') {
+   $limit="LIMIT 50";
+  }
+
+  global $limit;
+  if ($limit) {
+   $this->limit=$limit;
+  }
+
+  if ($this->limit) {
+   $limit="LIMIT ".$this->limit;
+  } else {
+   $limit="LIMIT 50";
+  }
+
+  $limit=str_replace('LIMIT LIMIT', 'LIMIT', $limit);
+
+  $out['LIMIT']=$this->limit;
+
+//  $res=SQLSelect("SELECT shouts.*, DATE_FORMAT(shouts.ADDED, '%H:%i') as DAT, TO_DAYS(shouts.ADDED) as DT, users.NAME, users.HOST FROM shouts LEFT JOIN users ON shouts.MEMBER_ID=users.ID WHERE $qry ORDER BY shouts.ADDED DESC, ID DESC $limit");
+  $res=SQLSelect("SELECT shouts.*, DATE_FORMAT(shouts.ADDED, '%H:%i') as DAT, TO_DAYS(shouts.ADDED) as DT, users.NAME, users.COLOR, users.AVATAR FROM shouts LEFT JOIN users ON shouts.MEMBER_ID=users.ID ORDER BY shouts.ADDED DESC, ID DESC $limit");
+
+//  if ($_GET['reverse']) {
+   $this->reverse=1;
+//  }
+
+  if (!$this->reverse) {
+   $res=array_reverse($res);
+  } else {
+   $out['REVERSE']=1;
+  }
+//  $txtdata="<h4>".day2str(date('w')).date(' d ').mon2str(date('m')).date(' Y')." года</h4><p>";
+
+  if ($this->mobile) {
+   $out['MOBILE']=1;
+  }
+
+
+  if ($res[0]['ID']) {
+   $old_dt=$res[0]['DT'];
+   $total=count($res);
+   for($i=0;$i<$total;$i++) {
+
+    // some action for every record if required
+    $tmp=explode(' ', $res[$i]['ADDED']);
+    $res[$i]['ADDED']=fromDBDate($tmp[0])." ".$tmp[1];
+    if ($res[$i]['DT']!=$old_dt) {
+        $txtdata.="<h4>".day2str(date('w',strtotime($tmp[0]))).date(' d ',strtotime($tmp[0])).mon2str(date('m',strtotime($tmp[0]))).date(' Y',strtotime($tmp[0]))." года</h4>";
+        $old_dt=$res[$i]['DT'];
+    }
+    if ($res[$i]['MEMBER_ID']==0) {
+     $res[$i]['NAME']='Алиса';
+     $res[$i]['AVATAR']='10_14045.png';
+	 $left_right="class=\"left clearfix\"";
+	 $img_left_right="<span class=\"chat-img pull-left\">";
+	 $time_block="<small class=\"pull-right text-muted\">";
+	 $nic_block="class=\"primary-font\"";	 
+    }else{
+	 $left_right="class=\"right clearfix\"";
+	 $img_left_right="<span class=\"chat-img pull-right\">";
+	 $time_block="<small class=\"text-muted\">";
+	 $nic_block="class=\"pull-right primary-font\"";
+		
+	}
+
+	$txtdata.="
+				<ul class=\"chat\">
+					<li ".$left_right.">";
+	if ($res[$i]['AVATAR']!=''){
+	    $txtdata.="".
+					$img_left_right."
+						<img src='/cms/avatars/".$res[$i]['AVATAR']."' class=\"img-circle\" width=\"40\" height=\"40\">
+					</span>";
+	}else{
+	    $txtdata.="".		
+					$img_left_right."
+						<img src='/img/icons/user.png' class=\"img-circle\" width=\"40\" height=\"40\">
+					</span>";		
+	}
+	$txtdata.="
+					<div class=\"chat-body clearfix\">
+						<div class=\"header\">
+							".$time_block."
+								<span class=\"glyphicon glyphicon-time\"></span> ".$res[$i]['DAT']."
+							</small>";
+
+	$txtdata.="			   
+							<strong ".$nic_block."> ".$res[$i]['NAME']."
+							</strong>
+						</div>";							
+	$txtdata.="			
+						<h5>".$res[$i]['COLOR']." ".$lr." ".nl2br($res[$i]['MESSAGE'])."
+						</h5>
+                    </div>
+                    </li>
+				</ul>";
+
+   }
+
+   $txtdata.='';
+   $out['RESULT']=$res;
+   $out['TXT_DATA']=$txtdata;
+
+  } else {
+   $txtdata.='No data';
+  }
+   $txtdata.='';
+  $out['SERVER_NAME']=$_SERVER['SERVER_NAME'];
+
+  if ($this->action=='' && $getdata!='') {
+   header ("HTTP/1.0: 200 OK\n");
+   header ('Content-Type: text/html; charset=utf-8');
+   echo $txtdata;
+   $session->save();
+   exit;
+  }
+
+function mon2str($mon){
+    switch ($mon){
+	case 01: $m="января";break;
+	case 02: $m="февраля";break;
+	case 03: $m="марта";break;
+	case 04: $m="апреля";break;
+	case 05: $m="мая";break;
+	case 06: $m="июня";break;
+	case 07: $m="июля";break;
+	case 08: $m="августа";break;
+	case 09: $m="сентября";break;
+	case 10: $m="октября";break;
+	case 11: $m="ноября";break;
+	case 12: $m="декабря";break;
+    }
+return $m;
+}
+function day2str($day){
+    switch ($day){
+	case 1: $m="Понедельник";break;
+	case 2: $m="Вторник";break;
+	case 3: $m="Среда";break;
+	case 4: $m="Четверг";break;
+	case 5: $m="Пятница";break;
+	case 6: $m="Суббота";break;
+	case 0: $m="Воскресенье";break;
+    }
+return $m;
+}
+
+?>
