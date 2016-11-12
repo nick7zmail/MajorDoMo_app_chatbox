46069f30289fc338fa63c9bb794eddf8b2e008de
nick7zmail <nick7zmail@gmail.com>
2016-11-12 15:12:00 +0500

    Изменена функция saydim()

--- lib/dym.php
+++ lib/dym.php
@@ -20,6 +20,7 @@ DebMes('SAY FUNC: '.$ph);
         $rec['ADDED']     = date('Y-m-d H:i:s');
         $rec['ROOM_ID']   = 0;
         $rec['MEMBER_ID'] = $member_id;
+        $rec['SOURCE'] = $source;
   
         if ($level>0) $rec['IMPORTANCE']=$level;
         
@@ -28,91 +29,40 @@ DebMes('SAY FUNC: '.$ph);
         if (defined('SETTINGS_HOOK_BEFORE_SAY') && SETTINGS_HOOK_BEFORE_SAY!='') {
          eval(SETTINGS_HOOK_BEFORE_SAY);
         }
-
         if ($level >= (int)getGlobal('minMsgLevel'))
         { 
-                //$voicemode!='off' && 
-
-           $lang='en';
-           if (defined('SETTINGS_SITE_LANGUAGE')) {
-                $lang=SETTINGS_SITE_LANGUAGE;
-           }
-           if (defined('SETTINGS_VOICE_LANGUAGE')) {
-                $lang=SETTINGS_VOICE_LANGUAGE;
-           }
-
-           if (!defined('SETTINGS_TTS_GOOGLE') || SETTINGS_TTS_GOOGLE) {
-                $google_file=GoogleTTS($ph, $lang);
-           } else {
-                $google_file=false;
-           }
 
            if (!defined('SETTINGS_SPEAK_SIGNAL') || SETTINGS_SPEAK_SIGNAL=='1') {
               $passed=SQLSelectOne("SELECT (UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(ADDED)) as PASSED FROM shouts WHERE ID!='".$rec['ID']."' ORDER BY ID DESC LIMIT 1");
               if ($passed['PASSED']>20) { // play intro-sound only if more than 30 seconds passed from the last one
-            if ($ding == 1){playSound('dingdong', 1, $level);}
-            if ($ding == 2){playSound('tone1', 1, $level);}
-            if ($ding == 3){playSound('tone2', 1, $level);}
-            if ($ding == 4){playSound('tone3', 1, $level);}
-                  }
-           }
-
-           if ($google_file) {
-                @touch($google_file);
-                        playSound($google_file, 1, $level);
-           } else {
-                safe_exec('cscript '.DOC_ROOT.'/rc/sapi.js '.$ph, 1, $level);
+                    if ($ding == 1){playSound('dingdong', 1, $level);}
+                    if ($ding == 2){playSound('tone1', 1, $level);}
+                    if ($ding == 3){playSound('tone2', 1, $level);}
+                    if ($ding == 4){playSound('tone3', 1, $level);}
+                }
            }
         }
-        global $ignorePushover;
-        if (defined('SETTINGS_PUSHOVER_USER_KEY') && SETTINGS_PUSHOVER_USER_KEY && !$ignorePushover) {
-                include_once(ROOT.'lib/pushover/pushover.inc.php');
-                if (defined('SETTINGS_PUSHOVER_LEVEL')){
-                        if($level>=SETTINGS_PUSHOVER_LEVEL) {
-                                postToPushover($ph);
-                        }
-                } elseif ($level>0) {
-                        postToPushover($ph);
-                }
-        }
+       setGlobal('lastSayTime', time());
+       setGlobal('lastSayMessage', $ph);
+       processSubscriptions('SAY', array('level' => $level, 'message' => $ph, 'member_id' => $member_id, 'ignoreVoice'=>$ignoreVoice));
 
-        global $ignorePushbullet;
-        if (defined('SETTINGS_PUSHBULLET_KEY') && SETTINGS_PUSHBULLET_KEY && !$ignorePushbullet) {
-                include_once(ROOT.'lib/pushbullet/pushbullet.inc.php');
-                if (defined('SETTINGS_PUSHBULLET_PREFIX') && SETTINGS_PUSHBULLET_PREFIX) {
-                 $prefix=SETTINGS_PUSHBULLET_PREFIX.' ';
-                } else {
-                 $prefix='';
-                }
+       if (!$noPatternMode)
+       {
+          include_once(DIR_MODULES . 'patterns/patterns.class.php');
+          $pt = new patterns();
+          $pt->checkAllPatterns($member_id);
+       }
 
-                if (defined('SETTINGS_PUSHBULLET_LEVEL')){
-                        if($level>=SETTINGS_PUSHBULLET_LEVEL) {
-                                postToPushbullet($prefix.$ph);
-                        }
-                } elseif ($level>0) {
-                        postToPushbullet($prefix.$ph);
-                }
-        }
+       if (defined('SETTINGS_HOOK_AFTER_SAY') && SETTINGS_HOOK_AFTER_SAY != '')
+       {
+            eval(SETTINGS_HOOK_AFTER_SAY);
+       }
 
-        global $ignoreGrowl;
-        if (defined('SETTINGS_GROWL_ENABLE') && SETTINGS_GROWL_ENABLE && $level>=SETTINGS_GROWL_LEVEL && !$ignoreGrowl) {
-         include_once(ROOT.'lib/growl/growl.gntp.php');
-         $growl = new Growl(SETTINGS_GROWL_HOST, SETTINGS_GROWL_PASSWORD);
-         $growl->setApplication('MajorDoMo','Notifications');
-         //$growl->registerApplication('http://localhost/img/logo.png');
-         $growl->notify($ph);
-        }
-
-        global $ignoreTwitter;
-        if (defined('SETTINGS_TWITTER_CKEY') && SETTINGS_TWITTER_CKEY && !$ignoreTwitter) {
-         postToTwitter($ph);
-        }
-
-        if (defined('SETTINGS_HOOK_AFTER_SAY') && SETTINGS_HOOK_AFTER_SAY!='') {
-         eval(SETTINGS_HOOK_AFTER_SAY);
-        }
-
-
+       $terminals=SQLSelect("SELECT NAME FROM terminals WHERE IS_ONLINE=1 AND MAJORDROID_API=1");
+       $total=count($terminals);
+       for($i=0;$i<$total;$i++) {
+            sayTo($ph, $level, $terminals[$i]['NAME']);
+       }
  }
 
 ?>
